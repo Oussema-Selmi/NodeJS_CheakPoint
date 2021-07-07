56c992ff9b2a30bd8bf629b522240efc9dc79fee
Oussema Selmi <oussemaselmi1901@gmail.com>
2021-07-07 08:22:22 +0100

    nodejs

---
+++ my-first-async-io.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const file = process.argv[2];
+
+fs.readFile(file, "utf8", function (err, contents) {
+  if (err) {
+    return console.error(err);
+  }
+  const lines = contents.split("\n").length - 1;
+  console.log(lines);
+});
